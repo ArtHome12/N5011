7237ab5e13c12404b90515961511d0d8a389be42
ArtHome12 <_mag12@yahoo.com>
2021-03-16 20:00:37 +0700

    1

--- src/states.rs
+++ src/states.rs
@@ -34,11 +34,17 @@ impl Default for Dialogue {
 pub struct StartState;
 
 #[teloxide(subtransition)]
-async fn start(_state: StartState, cx: TransitionIn, _ans: String,) -> TransitionOut<Dialogue> {
+async fn start(state: StartState, cx: TransitionIn, _ans: String,) -> TransitionOut<Dialogue> {
+   // Extract user id
+   let user = cx.update.from();
+   if user.is_none() {
+      cx.answer_str("Error, no user");
+      return next(state);
+   }
 
    // For admin and regular users there is different interface
-   let user = cx.update.from();
-   let is_admin = if user.is_some() {db::is_admin(user.unwrap().id)} else {false};
+   let user_id = user.unwrap().id;
+   let is_admin = db::is_admin(user_id);
 
    // Prepare menu
    let commands = if is_admin {
@@ -57,19 +63,41 @@ async fn start(_state: StartState, cx: TransitionIn, _ans: String,) -> Transitio
    .reply_markup(ReplyMarkup::ReplyKeyboardMarkup(markup))
    .send()
    .await?;
-   next(SelectCommandState)
+   next(SelectCommandState { user_id, is_admin })
 }
 
-pub struct SelectCommandState;
+pub struct SelectCommandState {
+   user_id: i32,
+   is_admin: bool,
+}
 
 #[teloxide(subtransition)]
 async fn select_command(
-   _state: SelectCommandState,
+   state: SelectCommandState,
    cx: TransitionIn,
    ans: String,
 ) -> TransitionOut<Dialogue> {
-   cx.answer_str(format!("Selected {}", ans)).await?;
-   next(StartState)
+   // Handle commands
+   if ans == "Изменить ориджин" {
+      // Collect info about update
+      let descr = db::user_descr(state.user_id).await;
+      let descr = format!("Ваш текущий ориджин {}.\n Пожалуйста, введите строку вида 2:5011/102,Fips_BBS,Ufa,Artem_G.Khomenko или просто /, чтобы оставить текущую информацию без изменений", descr);
+
+      let markup = ReplyKeyboardMarkup::default()
+      .append_row(vec![KeyboardButton::new("/")])
+      .resize_keyboard(true);
+
+      cx.answer(descr)
+      .reply_markup(ReplyMarkup::ReplyKeyboardMarkup(markup))
+      .send().
+      await?;
+      next(state)
+   } else {
+         cx.answer_str(format!("Неизвестная команда {}", ans)).await?;
+
+         // Stay in previous state
+         next(state)
+   }
 }
 
 

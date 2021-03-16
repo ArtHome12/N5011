42d731490529ed947a666f69a00543495f482f84
ArtHome12 <_mag12@yahoo.com>
2021-03-16 21:49:57 +0700

    1

--- src/database.rs
+++ src/database.rs
@@ -31,8 +31,8 @@ pub async fn announcement(user_id: i32, time: i32, def_descr: &str) -> Option<St
 
    match load_user(user_id).await {
       Some(user) => {
-         // If enough time has passed
-         if time - user.last_seen > 30 {
+         // If enough time has passed and origin was changed
+         if time - user.last_seen > 30 && user.descr != def_descr {
             update_user_time(user_id, time).await;
             Some(user.descr)
          } else {
